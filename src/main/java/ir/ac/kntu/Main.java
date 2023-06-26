43938aa09f6ae4317a4300d2ba53f66a9c0416fa
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2023-06-26 14:26:12 +0330

    add Description in Main.java

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -4,11 +4,25 @@ import ir.ac.kntu.HelperClasses.DefaultData;
 import ir.ac.kntu.menu.Auth.AuthMenu;
 import ir.ac.kntu.models.Store;
 
+/**
+ * @author Mohammad hosein Shabaniraad
+ */
+
 public class Main {
     public static void main(String[] args) {
-        //Store storeDB = DefaultData.addDefaultData();
-        //DAOStore.write(storeDB);
-        Store storeDB = DAOStore.read();
+
+        Store storeDB = null;
+
+        /**
+         * add stroe from defaultData class
+         */
+        storeDB = DefaultData.addDefaultData();
+        DAOStore.write(storeDB);
+        /**
+         * add store from DataBase file
+         */
+        storeDB = DAOStore.read();
+
         AuthMenu authMenu = new AuthMenu(storeDB);
         authMenu.showMenu();
         DAOStore.write(storeDB);
